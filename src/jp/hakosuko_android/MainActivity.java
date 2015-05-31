2c1d47c07e8e3c57722a97670cc4bf303b305a90
Your Name <you@example.com>
2015-05-31 20:57:44 +0900

    初回ソース

--- src/jp/hakosuko_android/MainActivity.java
+++ src/jp/hakosuko_android/MainActivity.java
@@ -5,11 +5,11 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.webkit.WebView;
 
 public class MainActivity extends Activity  {
-	private WebView webView = null;
-	private static final Uri URL_STHOME = Uri.parse("http://192.168.100.104:9000/index.html");
+	//private WebView webView = null;
+	//private static final Uri URL_STHOME = Uri.parse("http://192.168.100.104:9000/index.html");
+	private static final Uri URL_STHOME = Uri.parse("http://192.168.158.36:9000/index.html");
 
 	/**
 	 * アプリ起動時に最初に呼ばれるメソッド(onCreate)
@@ -20,7 +20,14 @@ public class MainActivity extends Activity  {
 
 		//setContentView(R.layout.activity_main); // activity_browser.xmlをセット
 		//webView = (WebView) findViewById(R.id.webview);
-		//webView.loadUrl("http://192.168.100.104:9000/index.html"); 	// サイトの読み込み
+
+		// webview設定
+		//WebSettings websettings = webView.getSettings();
+
+		// javaScript有効
+		//websettings.setJavaScriptEnabled(true);
+
+		//webView.loadUrl("http://192.168.158.36:9000/index.html"); 	// サイトの読み込み
 
 		Intent web = new Intent(Intent.ACTION_VIEW, URL_STHOME);
 		startActivity(web);
